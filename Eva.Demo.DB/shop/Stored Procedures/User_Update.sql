349970d2e2a067d1ab68b7636c18e705a1830c63
Eva Zhang <evacheck@163.com>
2018-11-16 23:47:52 +0800

    upd

---
+++ Eva.Demo.DB/shop/Stored Procedures/User_Update.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE shop.User#Update
+(
+	@userID bigint, @surname Alias, @phone Phone, @email Email, @country Country,
+	@state   Alias, @city    Alias, @address1 Address, @address2 Address
+)
+AS
+BEGIN
+	SET NOCOUNT    ON;
+
+	exec	core.User#Update @userID=@userID, @surname=@surname, @phone=@phone, @email=@email, @country=@country,
+							 @state =@state,  @city=@city, @address1=@address1, @address2=@address2
+
+END
