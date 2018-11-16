3f524f0d15c333646c815c13f431f637d9ea1aa9
Eva Zhang <evacheck@163.com>
2018-11-16 22:56:42 +0800

    update and renaming

--- Eva.Demo.DB/core/Stored Procedures/User_Add.sql
+++ Eva.Demo.DB/core/Stored Procedures/User_Add.sql
@@ -1,14 +1,32 @@
 ﻿--Eva
 CREATE PROCEDURE core.User#Add
 (
-	@type    tinyint, @name  Alias, @surName Alias, @phone      Phone, @email    Email,
-	@country Country, @state Alias, @city    Alias, @address1 Address, @address2 Address
+	@type    tinyint, @name  Alias, @surname Alias, @phone      Phone, @email    Email,
+	@country Country, @state Alias, @city    Alias, @address1 Address, @address2 Address,
+	@roleIDs nvarchar(max)
 )
 AS
 BEGIN
-	SET NOCOUNT ON;
+	SET NOCOUNT    ON;
+	SET XACT_ABORT ON;
 
-	insert	core._User
-			( Type,  Name, Surname,   Phone,  Email,  Country,  State,  City,  Address1, Address2)
-	values	(@type, @name, @surName, @phone, @email, @country, @state, @city, @address1, @address2)
+	BEGIN TRY
+		BEGIN	TRAN;
+
+		declare	@userID bigint;
+		select	@userID=next value for UserSeq;
+		insert	core._User
+		(		     ID,  Type,  Name, Surname,   Phone,  Email,  Country,  State,  City,  Address1, Address2)
+		select	@userID, @type, @name, @surname, @phone, @email, @country, @state, @city, @address1, @address2;
+
+		insert	auth._UserRole
+		(		 UserID, RoleID)
+		select	@userID, Value
+		from	loc.Comma#Slice(@roleIDs)
+		--
+		COMMIT	TRAN;
+	END TRY
+	BEGIN CATCH
+		if (xact_state() = -1) ROLLBACK TRAN; throw;
+	END CATCH
 END
---
+++ Eva.Demo.DB/core/Stored Procedures/User_Insert.sql
@@ -0,0 +1,14 @@
+﻿--Eva
+CREATE PROCEDURE core.User#Insert
+(
+	@type    tinyint, @name  Alias, @surName Alias, @phone      Phone, @email    Email,
+	@country Country, @state Alias, @city    Alias, @address1 Address, @address2 Address
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	insert	core._User
+			( Type,  Name, Surname,   Phone,  Email,  Country,  State,  City,  Address1, Address2)
+	values	(@type, @name, @surName, @phone, @email, @country, @state, @city, @address1, @address2)
+END
