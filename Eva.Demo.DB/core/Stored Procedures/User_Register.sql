28acfd4b340e575251a596a42b0bf8b6e16a4cc8
Eva Zhang <evacheck@163.com>
2018-11-16 23:09:27 +0800

    update register

---
+++ Eva.Demo.DB/core/Stored Procedures/User_Register.sql
@@ -0,0 +1,35 @@
+﻿--Eva
+CREATE PROCEDURE core.User#Register
+(
+	@name  Alias, @pwd Pwd
+)
+AS
+BEGIN
+	SET NOCOUNT    ON;
+	SET XACT_ABORT ON;
+
+	BEGIN TRY
+		BEGIN	TRAN;
+
+		declare	@userID bigint
+		,		@type   tinyint=(select Customer from core.User#Type());
+		select	@userID=next value for UserSeq;
+		insert	core._User
+		(		     ID,  Type,  Name)
+		select	@userID, @type, @name;
+
+		insert	auth._Authentication
+		(		UserName, Password, UserID)
+		select	   @name,     @pwd, @userID;
+
+		declare	@roleID int=(select Buyer from auth.Role#ID());
+		insert	auth._UserRole
+		(		 UserID, RoleID)
+		select	@userID, @roleID;
+		--
+		COMMIT	TRAN;
+	END TRY
+	BEGIN CATCH
+		if (xact_state() = -1) ROLLBACK TRAN; throw;
+	END CATCH
+END
--- Eva.Demo.DB/core/Tables/_User.sql
+++ Eva.Demo.DB/core/Tables/_User.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [core].[_User] (
-    [ID]       BIGINT         NOT NULL IDENTITY(1000, 1),
+    [ID]       BIGINT         NOT NULL,
     [Type]     TINYINT        NOT NULL,
     [Name]     NVARCHAR (20)  NOT NULL,
-    [Surname]  NVARCHAR (20)  NOT NULL,
-    [Phone]    VARCHAR (20)   NOT NULL,
-    [Email]    NVARCHAR (40)  NOT NULL,
-    [Country]  CHAR (2)       NOT NULL,
-    [State]    NVARCHAR (20)  NOT NULL,
-    [City]     NVARCHAR (20)  NOT NULL,
-    [Address1] NVARCHAR (100) NOT NULL,
-    [Address2] NVARCHAR (100) NOT NULL,
+    [Surname]  NVARCHAR (20)  NOT NULL default(N''),
+    [Phone]    VARCHAR (20)   NOT NULL default(N''),
+    [Email]    NVARCHAR (40)  NOT NULL default(N''),
+    [Country]  CHAR (2)       NOT NULL default(N''),
+    [State]    NVARCHAR (20)  NOT NULL default(N''),
+    [City]     NVARCHAR (20)  NOT NULL default(N''),
+    [Address1] NVARCHAR (100) NOT NULL default(N''),
+    [Address2] NVARCHAR (100) NOT NULL default(N''),
     CONSTRAINT [PK__User] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
