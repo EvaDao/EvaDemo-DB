a22ec084a65247d6d4983a27460a82bb8e6a6927
Eva Zhang <evacheck@163.com>
2018-11-19 23:12:05 +0800

    add init data

--- Eva.Demo.DB/core/Stored Procedures/User_Add.sql
+++ Eva.Demo.DB/core/Stored Procedures/User_Add.sql
@@ -13,11 +13,15 @@ BEGIN
 	BEGIN TRY
 		BEGIN	TRAN;
 
-		declare	@userID bigint;
+		declare	@userID bigint, @pwd nvarchar(100)=N'e397433ba52b69656be325c89581b13a';-- abc123_
 		select	@userID=next value for UserSeq;
 		insert	core._User
 		(		     ID,  Type,  Name, Surname,   Phone,  Email,  Country,  State,  City,  Address1, Address2)
 		select	@userID, @type, @name, @surname, @phone, @email, @country, @state, @city, @address1, @address2;
+		
+		insert	auth._Authentication
+		(		UserName, Password, UserID)
+		select	   @name,     @pwd, @userID;
 
 		insert	auth._UserRole
 		(		 UserID, RoleID)
---
+++ Eva.Demo.DB/initData/initData.user.admin.sql
@@ -0,0 +1,9 @@
+﻿declare
+@type    tinyint=2, @name  Alias=N'admin', @surname Alias=N'admin', @phone Phone=N'1561***1234', @email Email=N'evacheck@163.com',
+@country Country=N'CN', @state Alias=N'Shanghai', @city Alias=N'Shanghai', @address1 Address=N'Changzhong road', @address2 Address=N'',
+@roleIDs nvarchar(max)=N'102';
+
+exec	core.User#Add
+@type=@type, @name=@name, @surname=@surname, @phone=@phone, @email=@email,
+@country=@country, @state=@state, @city=@city, @address1=@address1,@address2=@address2,
+@roleIDs=@roleIDs;
